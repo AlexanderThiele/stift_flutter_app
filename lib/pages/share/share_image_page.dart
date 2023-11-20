cf87aed3deb00c5f5f8a179b943dc545df6e41f5
alexanderthiele <alex@thiele.dev>
2023-11-20 16:38:13 +0100

    add progressIndicator to share button

--- lib/pages/share/share_image_page.dart
+++ lib/pages/share/share_image_page.dart
@@ -14,18 +14,31 @@ import 'package:pencalendar/repository/repository_provider.dart';
 import 'package:responsive_spacing/responsive_spacing.dart';
 import 'package:share_plus/share_plus.dart';
 
-class ShareImagePage extends ConsumerWidget {
+class ShareImagePage extends ConsumerStatefulWidget {
+  const ShareImagePage({super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _ShareImagePage();
+}
+
+class _ShareImagePage extends ConsumerState<ShareImagePage> {
   final GlobalKey globalKey = GlobalKey();
 
-  ShareImagePage({super.key});
+  bool buttonLoading = false;
 
   _shareCalendar(Rect position) async {
+    setState(() {
+      buttonLoading = true;
+    });
     final picture = await _captureWidget();
 
     await Share.shareXFiles(
       [XFile.fromData(picture, mimeType: 'image/png', name: "Stift Calendar App", lastModified: DateTime.now())],
       sharePositionOrigin: position,
     );
+    setState(() {
+      buttonLoading = false;
+    });
   }
 
   Future<Uint8List> _captureWidget() async {
@@ -40,7 +53,7 @@ class ShareImagePage extends ConsumerWidget {
   }
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  Widget build(BuildContext context) {
     final int selectedYear = ref.watch(activeCalendarYearProvider);
     final publicHolidays = ref.watch(publicHolidayControllerProvider);
     return ResponsiveScaffold(
@@ -75,8 +88,14 @@ class ShareImagePage extends ConsumerWidget {
 
               final shareCard = Builder(builder: (context) {
                 return ElevatedButton(
-                  child: Text(context.l10n.shareNow),
+                  child: switch (buttonLoading) {
+                    true => const SizedBox(width: 16, height: 16, child: CircularProgressIndicator()),
+                    false => Text(context.l10n.shareNow)
+                  },
                   onPressed: () {
+                    if (buttonLoading) {
+                      return;
+                    }
                     ref.read(analyticsRepositoryProvider).trackEvent(AnalyticEvent.shareCalendar);
                     final box = context.findRenderObject() as RenderBox?;
                     _shareCalendar(box!.localToGlobal(Offset.zero) & box.size);
